fb161064ac99fd905db8ec2eab4421ac280f8eec
barakatokikiola <barakattajudeen2018@gmail.com>
2024-10-29 15:22:37 +0100

    social link updated in navbar

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,9 +8,9 @@ const Hero =() => {
     return(
         <div className="flex flex-col lg:flex-row">
           
-            <div className="p-10 lg:p-20 bg-burnt-sienna text-lumber w-full lg:w-2/3">
-                <h1 className="text-6xl font-bold"><span className="text-white">Front</span>End</h1>
-                <h1 className="text-6xl font-bold text-saffron">Developer.</h1>
+            <div className="p-10 lg:p-20 bg-burnt-sienna text-saffron w-full lg:w-2/3">
+                <h1 className="text-6xl font-bold text-off-white">FrontEnd</h1>
+                <h1 className="text-6xl font-bold ">Developer.</h1>
                 <p className="my-5 lg:w-1/2 w-full text-justify text-off-white">Hi, I'm Barakat, a passionate Frontend Developer with a knack for turning ideas into interactive, 
                     user-friendly web experiences. With a strong foundation in HTML, CSS, JavaScript, and modern frameworks like React and Next.js,
                      I craft responsive, intuitive, and visually appealing websites. Explore my work, 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,8 @@
 import React from "react";
-import logo from '../assets/Barakat Okikiola.svg';
+import logo from '../assets/barakat.png';
 import { CiMenuFries } from "react-icons/ci";
-import { IoMdClose } from "react-icons/io";
-// import { CiDark, CiLight } from "react-icons/ci";
+
+
 
 
 const Navbar=()=>{
@@ -17,11 +17,23 @@ const Navbar=()=>{
                 <CiLight className=" swap-off fill-current text-4xl"/>
                 <CiDark  className=" swap-on fill-current text-4xl"/>
             </label> */}
-            <label className=" swap swap-rotate">
-                <input type="checkbox" />
-                <CiMenuFries  className=" swap-off fill-current text-4xl"/>
-                <IoMdClose  className=" swap-on fill-current text-4xl"/>
-            </label>
+
+<details className="dropdown dropdown-end">
+        <summary tabIndex={0} role="button" className="btn bg-transparent border-none hover:bg-sandy-brown rounded-btn">
+        
+                <CiMenuFries  className="text-4xl"/>
+            
+            </summary>
+        <ul
+          tabIndex={0}
+          className="menu dropdown-content bg-base-100 rounded-box z-[1] mt-4 w-52 p-2 shadow">
+          <li><a href="https://www.x.com/barakatokikiola" target="_blank" rel="noreferrer">Twitter</a></li>
+          <li><a href="https://www.github.com/barakatokikiola" target="_blank" rel="noreferrer">GitHub</a></li>
+          <li><a href="https://www.linkedin.com/in/barakatokikiola" target="_blank" rel="noreferrer">Linkedin</a></li>
+            
+        </ul>
+      </details>
+
         </div>
     )
 }
