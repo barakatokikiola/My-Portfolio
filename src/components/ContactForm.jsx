66d5c11fc1cd0652807dda8236c87dcf155c6cda
barakatokikiola <barakattajudeen2018@gmail.com>
2024-12-22 14:32:55 +0100

    formspree added to deliver message

---
+++ src/components/ContactForm.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { useForm} from '@formspree/react';
+
+const ContactForm = () => {
+    const [state, handleSubmit] = useForm("mgvvwbrp");
+    if (state.succeeded) {
+        return( <p className='flex justify-center text-2xl my-auto w-full h-full text-pink-700 font-semibold'>Thanks for reaching out!</p>);
+    }
+  return (
+    <form onSubmit={handleSubmit} class="flex flex-col w-full">  
+  <div class="grid md:grid-cols-2 md:gap-6">
+    <div class="relative z-0 w-full mb-5 group">
+        <input type="text" name="name" id="name" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+        <label for="floating_name" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Your Name</label>
+    </div>
+    <div class="relative z-0 w-full mb-5 group">
+        <input type="mail" name="email" id="email" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+        <label for="floating_email" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Your Mail</label>
+    </div>
+  </div>
+  <div class="relative z-0 w-full mb-5 group">
+      <input type="text" name="message" id="message" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+      <label for="floating_message" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Message</label>
+  </div>
+  <button type="submit" className="text-white bg-pink-600/60 hover:bg-pink-600 font-medium rounded-lg text-sm sm:w-auto  py-2.5 text-center">Submit Message</button>
+</form>
+  )
+}
+
+export default ContactForm
