78e0fcc15267cac3b5cdc3e07b80c56d56c43883
barakatokikiola <barakattajudeen2018@gmail.com>
2025-05-19 15:10:56 +0100

    hero section ui updated

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,30 +1,49 @@
 import React from "react";
-import barakat from '../assets/Component 2.png';
+import barakat from '../assets/Hero.png';
 import Background from "./Background";
+import angular_  from '../assets/angular.png';
+import react_ from '../icons/reactjs.svg';
+import next_ from '../icons/nextjs.svg';
 
 
 
 const Hero =() => {
 
     return(
-        <div id='hero-section' className="flex flex-col  md:pt-24 md:flex-row justify-between z-10">
+        <div id='hero-section' className="flex flex-col  md:pt-24 md:flex-row justify-between z-10 place-items-center gap-y-8">
             
           
-            <div className="p-4 md:p-16 w-full text-[#000]">
+            <div className="p-4 md:p-16 w-full text-[#000] ">
             
                 <div className="md:text-start text-center text-purple-900">
-                <h1 className="text-4xl md:text-6xl font-bold font-cormorant">FRONTEND</h1>
-                <h1 className="text-4xl md:text-6xl font-cormorant">DEVELOPER.</h1>
+                <h1 className="text-4xl md:text-[5.5rem]/[7rem] font-bold font-cormorant">FRONTEND</h1>
+                <h1 className="text-4xl md:text-[5.5rem]/[7rem] font-cormorant">DEVELOPER.</h1>
                 </div>
-                <p className="my-4 w-full text-justify p-4">Hi, I'm Barakat, a passionate Frontend Developer with a knack for turning ideas into interactive, 
+                <p className="my-4 w-full text-lg/9 text-justify md:pr-[9rem] md:px-0 px-4">Hi, I'm Barakat, a passionate Frontend Developer with a knack for turning ideas into interactive, 
                     user-friendly web experiences. With a strong foundation in HTML, CSS, JavaScript, and modern frameworks like <span className="font-bold text-purple-900">React</span> and <span className="font-bold text-pink-600">Next.js</span>,
                      I craft responsive, intuitive, and visually appealing websites. Explore my work, 
                      and let's build something amazing together!</p>
+                     <div className="skills flex gap-x-4 mt-[3.5rem]">
+                        <div>
+                            <img src={angular_} alt="angular"/>
+                        </div>
+                       
+                        <div>
+                            <img src={react_} alt="react"/>
+                        </div>
+                         <div>
+                            <img src={next_} alt="next"/>
+                         </div>
+                     </div>
                      
             </div>
-            <div className="px-8 w-full mx-auto justify-center place-content-center bg-purple-900/25 ">
+            <div className="px-8 w-full mx-auto justify-center place-content-center bg-purple-900/15">
             <Background/>
                 <img src={barakat} alt='' className="md:ml-[-8rem]"  />
+            {/* <picture>
+            <source srcset="/images/hero.webp" type="image/webp"></source>
+            <img src={barakat} alt="Hero" />
+            </picture> */}
             </div>
            
         </div>
